b08eec96eda4326794fd9a69a4d14a642fa8f5de
Craig Drummond <craig.p.drummond@gmail.com>
2020-06-18 12:21:41 +0100

    Add fallback for clipboard copy

--- MaterialSkin/HTML/material/html/js/utils.js
+++ MaterialSkin/HTML/material/html/js/utils.js
@@ -928,9 +928,24 @@ function formatTrackNum(item) {
 }
 
 function copyTextToClipboard(text) {
-    if (!navigator.clipboard) {
-        fallbackCopyTextToClipboard(text);
-        return;
+    if (navigator.clipboard) {
+        navigator.clipboard.writeText(text);
+    } else {
+        var textArea = document.createElement("textarea");
+        textArea.value = text;
+
+        // Avoid scrolling to bottom
+        textArea.style.top = "0";
+        textArea.style.left = "0";
+        textArea.style.position = "fixed";
+
+        document.body.appendChild(textArea);
+        textArea.focus();
+        textArea.select();
+        try {
+            document.execCommand('copy');
+        } catch (err) {
+        }
+        document.body.removeChild(textArea);
     }
-    navigator.clipboard.writeText(text);
 }
